6f4646047baf0ac6b85415806b1dfae8739ac399
Phu Nguyen <phu.nguyen09995@gmail.com>
2018-04-19 23:54:34 +1200

    test idnent chat

--- index.js
+++ index.js
@@ -56,3 +56,16 @@ app.post('/echo', function(req, res) {
     source: 'webhook-echo-sample'
   });
 });
+app.post('/chat', function(req, res) {
+  var speech =
+    req.body.result &&
+    req.body.result.parameters &&
+    req.body.result.parameters.library
+      ? req.body.result.parameters.library
+      : 'Seems like some problem. Speak again.';
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: 'webhook-echo-sample'
+  });
+});
