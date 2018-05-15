36875fbf29ffd528e4803b605cfe968ae3c1bfcb
Phu Nguyen <phu.nguyen09995@gmail.com>
2018-05-16 00:32:26 +1200

    edit user work's flow

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,28 @@
 let apiai = require('apiai');
 const mongoose = require('mongoose');
+const isEmpty = require('../utils/isEmpty');
 
 const  Courses  = require('../model/Course');
-handleInputUnknow=(response)=>{
-  console.log(response);
-  console.log(response.result.contexts[0]);
-  if(response.result.action==='input.unknown' && response.result.contexts.length>0 && response.result.contexts[0].name==='awaiting_major'){
+let lifespanMajor=10;
+let lifeSpanAction=10;
+let userMajor;
+handleInputUnknowMajor=(response)=>{
+  if(response.result.action==='input.unknown' && response.result.contexts.length>0 && response.result.contexts[0].name==='awaiting_major' && lifespanMajor>0 ||response.result.action==='input.unknown' && lifespanMajor>0&&response.result.contexts.length>0 && response.result.contexts[0].name==='awaiting_major' && (isEmpty(response.result.contexts[0].action  )) ){
+    lifespanMajor--;
     return true;
   }
   else{
     return false;
   }
 }
+handleInputUnKnow=(response)=>{
+  if(response.result.action==='input.unknown' && response.result.contexts.length>0){
+    lifespanMajor=0;
+    return true;
+  }
+}
 handleInputUnknowGreetings=(response)=>{
-if(response.result.action==='input.unknown'){
-  console.log('vo day');
+if(response.result.action==='input.unknown' &&isEmpty(response.result.contexts)){
   return true;
 }
 else{
@@ -22,7 +30,6 @@ else{
 }
 }
 exports.processRequest = (req, res) => {
-  console.log(req.body);
   let bot = apiai('c621bac4072a4647bb9ecc2b2a0bad87');
 
   const message = req.body.message;
@@ -31,7 +38,7 @@ exports.processRequest = (req, res) => {
   });
   request.on('response', async response => {
     let singleMajor;
-    if(handleInputUnknow(response)){
+    if(handleInputUnknowMajor(response)){
       res.json({
         message:'Please provide your major',
         userSent:message
@@ -43,108 +50,74 @@ exports.processRequest = (req, res) => {
         userSent:message
       })
     }
-    if (
-      response.result.parameters.action &&
-      response.result.contexts[0].parameters.givenMajor
-    ) {
+    if(handleInputUnKnow(response)){
+      res.json({
+        message:'Sorry I dont understand',
+        userSent:message
+      })
+    }
+    if (!isEmpty(userMajor) && lifeSpanAction > 0 && !isEmpty(response.result.parameters))
+    {
       let actionGiven;
-      if (response.result.parameters.action.length > 0) {
-        actionGiven = response.result.parameters.action[1];
-        let majorGiven = response.result.contexts[0].parameters.givenMajor;
-        console.log(actionGiven);
-        if (actionGiven === 'elective') {
-          const listOfElectivePapers = await Courses.find({})
-            .where('nameOfMajor')
-            .equals(majorGiven)
-            .exec();
-          listOfElectivePapers.forEach(data => {
-            singleMajor = data;
-          });
-          res.json({
-            message: response.result.fulfillment.messages[0].speech,
-            userSent: message,
-            singleMajor,
-            actionGiven
-          });
-        } else if (actionGiven === 'prerequisites') {
-          console.log('ihihihihihhi')
-          let paperGiven = response.result.parameters.papers;
-          console.log(response);
-          let result = Courses.find({});
-          const listOfPrePaper = await Courses.find({})
-            .where('corePapers.name')
-            .equals(response.result.parameters.papers)
-            .exec();
-          if (listOfPrePaper.length > 0) {
-            listOfPrePaper.forEach(data => {
+        console.log(response);
+        if (response.result.parameters.action.length > 0) {
+          actionGiven = response.result.parameters.action[0];
+          if (actionGiven === 'elective') {
+            const listOfElectivePapers = await Courses.find({})
+              .where('nameOfMajor')
+              .equals(userMajor)
+              .exec();
+            listOfElectivePapers.forEach(data => {
               singleMajor = data;
             });
             res.json({
               message: response.result.fulfillment.messages[0].speech,
               userSent: message,
               singleMajor,
-              paperGiven,
               actionGiven
             });
-          } else {
-            res.json({
-              message: `Sorry, there is no ${paperGiven} in ${majorGiven}, please double check again`,
-              userSent: message
-            });
+          } else if (actionGiven === 'prerequisites') {
+            let paperGiven = response.result.parameters.papers;
+            let result = Courses.find({});
+            const listOfPrePaper = await Courses.find({})
+              .where('corePapers.name')
+              .equals(response.result.parameters.papers)
+              .exec();
+            if (listOfPrePaper.length > 0) {
+              listOfPrePaper.forEach(data => {
+                singleMajor = data;
+              });
+              res.json({
+                message: response.result.fulfillment.messages[0].speech,
+                userSent: message,
+                singleMajor,
+                paperGiven,
+                actionGiven
+              });
+            } else {
+              res.json({
+                message: `Sorry, there is no ${paperGiven} in ${userMajor}, please double check again`,
+                userSent: message
+              });
+            }
           }
-        }
+        
       }
-      actionGiven = response.result.parameters.action[0];
-      let majorGiven = response.result.contexts[0].parameters.givenMajor;
-      console.log(actionGiven);
-      if (actionGiven === 'elective') {
-        const listOfElectivePapers = await Courses.find({})
-          .where('nameOfMajor')
-          .equals(majorGiven)
-          .exec();
-        listOfElectivePapers.forEach(data => {
-          singleMajor = data;
-        });
-        res.json({
-          message: response.result.fulfillment.messages[0].speech,
-          userSent: message,
-          singleMajor,
-          actionGiven
-        });
-      } else if (actionGiven === 'prerequisites') {
-        let paperGiven = response.result.parameters.papers;
-        const listOfPrePaper = await Courses.find({})
-          .where('corePapers.name')
-          .equals(response.result.parameters.papers)
-          .exec();
-        if (listOfPrePaper.length > 0) {
-          listOfPrePaper.forEach(data => {
-            singleMajor = data;
-          });
-
-          res.json({
-            message: response.result.fulfillment.messages[0].speech,
-            userSent: message,
-            singleMajor,
-            paperGiven,
-            actionGiven
-          });
-        } else {
-          res.json({
-            message: `Sorry, there is no ${paperGiven} in ${majorGiven}, please double check again`,
-            userSent: message
-          });
-        }
-      }
-    } else if (response.result.action === 'given_major') {
-      response.result.fulfillment.messages[0].speech = `So your major is ${
-        response.result.parameters.givenMajor
-      }`;
-      res.json({
-        message: response.result.fulfillment.messages[0].speech,
-        userSent: message
-      });
-    } else {
+      
+    }
+    else if (response.result.action === 'given_major') {
+      console.log('vo day');
+     lifespanMajor=0;
+     userMajor=response.result.parameters.givenMajor;
+     response.result.fulfillment.messages[0].speech = `So your major is ${
+       response.result.parameters.givenMajor
+     }`; 
+     res.json({
+       message: response.result.fulfillment.messages[0].speech,
+       userSent: message
+     });
+   }
+      else if(isEmpty(response.result.parameters)) {
       res.json({
         message: response.result.fulfillment.messages[0].speech,
         userSent: message
@@ -153,7 +126,6 @@ exports.processRequest = (req, res) => {
   });
 
   request.on('error', function(error) {
-    console.log(error);
   });
 
   request.end();
--- model/Course.js
+++ model/Course.js
@@ -2,28 +2,39 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const majorSchema = new mongoose.Schema({
-  nameOfMajor: { type: String },
-  points: { type: Number },
-  corePapers: [
-    {
-      name: { type: String },
-      sku: { type: String },
-      level: { type: Number },
-      point: { type: Number },
-      prerequisites: { type: String },
-      corequisites: { type: String }
-    }
-  ],
-  electivePapers: [
-    {
-      name: { type: String },
-      sku: { type: String },
-      level: { type: Number },
-      point: { type: Number },
-      prerequisites: { type: String },
-      corequisites: { type: String }
-    }
-  ]
-});
+  nameOfMajor: {type: String},
+  description: {type: String},
+  points: {type: Number},
+  corePapers:[{
+    name: {type: String},
+    sku: {type: String},
+    level: {type: Number},
+    point: {type: Number},
+    prescriptor: {type: String},
+    year: {type: Number},
+    semester: {type: String},
+    specificJob: {type: String},
+    prerequisites: [{type: String}],
+    followingPaper: [{type: String}],
+    corequisites: [{type: String}],
+    
+  }],
+  electivePapers:[{
+    name: {type: String},
+    sku: {type: String},
+    level: {type: Number},
+    point: {type: Number},
+    prescriptor: {type: String},
+    year: {type: Number},
+    semester: {type: String},
+    specificJob: {type: String},
+    prerequisites: [{type: String}],
+    followingPaper: [{type: String}],
+    corequisites: [{type: String}],
+    
+  }],
+  careerOppotunities: [{type: String}],
+
+}) 
 
 module.exports = Course = mongoose.model('courses', majorSchema);
