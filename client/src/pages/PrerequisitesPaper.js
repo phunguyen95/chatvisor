d6a40391ded0de999ba25be208992ce180a640a0
Michelle Extross <michelleanneextross@yahoo.co.nz>
2018-05-23 19:24:49 +1200

    Changed the default response if the year of study is entered incorrectly

--- client/src/pages/PrerequisitesPaper.js
+++ client/src/pages/PrerequisitesPaper.js
@@ -8,10 +8,11 @@ export default class PrerequisitesPaper extends Component {
       <div>
         <div>
           {prerequisites.length > 0
-            ? prerequisites.map(item=><div key={item}>{item}</div>)
+            ? prerequisites.map(item=><div key={item}>Hi,{item}</div>)
             : 'This paper does not require prerequisites papeprs'}
         </div>
       </div>
     );
   }
 }
+ 
