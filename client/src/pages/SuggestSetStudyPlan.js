29243c4657335c12fdd5088469cb9ac520413ab3
Michelle Extross <michelleanneextross@yahoo.co.nz>
2018-05-23 19:46:56 +1200

    Fixed typo on SuggestSetStudyPlan

--- client/src/pages/SuggestSetStudyPlan.js
+++ client/src/pages/SuggestSetStudyPlan.js
@@ -3,10 +3,8 @@ import React, { Component } from 'react'
 export default class SuggestSetStudyPlan extends Component {
     render() {
         const {list} = this.props;
-        console.log(list)
         return (
             <div>
-                The list of papers suggested for 
                 {list.length>0?list.map(item=>
                 <p>{item.name}</p>):null}
             </div>
